7eb06243d12af4c0c778ff75f1b30ef42c179c8a
Alexey Chirkov <alexey.chirkov@itdt.ru>
2014-02-26 14:30:20 +0400

    dictionary.js content generating via JSONObject was added to prevent incorrect JSON values encoding

--- modules/com.alkacon.bootstrap.formatters/resources/system/modules/com.alkacon.bootstrap.formatters/search/dictionary.js
+++ modules/com.alkacon.bootstrap.formatters/resources/system/modules/com.alkacon.bootstrap.formatters/search/dictionary.js
@@ -1,8 +1,10 @@
 <%@page buffer="none" session="false" taglibs="c,cms"%><%--
+--%><%@page import="org.opencms.json.JSONObject"%><%--
 --%><% org.opencms.util.CmsRequestUtil.setNoCacheHeaders(response); %><%--
 --%><c:set var="locale"><cms:info property="opencms.request.locale" /></c:set><%--
---%>var GWTsearchUIDictionary = {
-<%  java.util.Locale locale = new java.util.Locale((String)pageContext.getAttribute("locale"));
+--%>var GWTsearchUIDictionary = <%--
+
+--%><%java.util.Locale locale = new java.util.Locale((String)pageContext.getAttribute("locale"));
 
     org.opencms.relations.CmsCategoryService srv = org.opencms.relations.CmsCategoryService.getInstance();
     org.opencms.jsp.CmsJspActionElement jae = new org.opencms.jsp.CmsJspActionElement(pageContext, request, response);
@@ -10,26 +12,27 @@
     String siteRoot = cmsO.getRequestContext().getSiteRoot() + "/";
     java.util.List<String> reps = srv.getCategoryRepositories(cmsO, siteRoot + "/_categories/");
     java.util.List<org.opencms.relations.CmsCategory> cats = srv.readCategoriesForRepositories(cmsO, "/", true, reps);
+
+    final JSONObject resultJSON = new JSONObject();
     for (org.opencms.relations.CmsCategory cat : cats) {
-        String val = "\"" + cat.getPath() + "\":'" + cat.getTitle() + "',";
-        out.println("\t" + val);
+        resultJSON.put(cat.getPath(), cat.getTitle());
     }
-    
+
     java.util.List<org.opencms.workplace.explorer.CmsExplorerTypeSettings> settings = org.opencms.main.OpenCms.getWorkplaceManager().getExplorerTypeSettings();
     for (org.opencms.workplace.explorer.CmsExplorerTypeSettings setting : settings) {
         String title = org.opencms.workplace.CmsWorkplaceMessages.getResourceTypeName(locale, setting.getName());
         if (title == null) {
             title = setting.getName();
         }
-        String val = "\"" + setting.getName() + "\":'" + title + "',";
-        out.println("\t" + val);
+        resultJSON.put(setting.getName(), title);
     }
-    
-    java.util.ResourceBundle bundle = org.opencms.i18n.CmsResourceBundleLoader.getBundle("com.alkacon.bootstrap.formatters.messages", locale);
+
+    java.util.ResourceBundle bundle = org.opencms.i18n.CmsResourceBundleLoader.getBundle("ru.itdt.opencms.bootstrap.formatters.messages", locale);
     for (String key : bundle.keySet()) {
         String value = bundle.getString(key);
-        String val = "\"" + key + "\":'" + value + "',";
-        out.println("\t" + val);
+        resultJSON.put(key, value);
     }
-    out.print("\t\"locale\":'" + locale.toString() + "'"); %> 
-};
+    resultJSON.put("locale", locale.toString());
+
+    out.println(resultJSON.toString()); %><%--
+--%>;
